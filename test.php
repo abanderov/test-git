93c6fd67cf232860a37361f7fe9eeec1129f9c46
abanderov <a_banderov@yahoo.com>
2018-02-10 13:01:27 +0200

    LoremIpsum

--- test.php
+++ test.php
@@ -1,3 +1,7 @@
 <?php echo "newnew"; ?>
 
 <?php echo "this is for the test branch"; ?>
+
+Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+Lorem ipsumLorem ipsumLorem ipsumLorem ipsum
+Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
